aba3b064a16067a1447cbb15a8a94630b3ee3a7d
andreymmonteiro <77641620+andreymmonteiro@users.noreply.github.com>
2021-10-08 16:27:35 -0300

    Paginação, configuração de Tasks e async await para melhorar performance

---
+++ SmartSchool.API/Helper/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace SmartSchool.API.Helper
+{
+    //Classe usada para adicionar as informações da paginação no Header da resposta da requisição
+    //Ela usa o PaginationHeader para construir o objeto e adicionar no Header
+    public static class Extensions
+    {
+        public static void AddPagination(this HttpResponse response, 
+                                            int CurrentPage, 
+                                            int TotalPages, 
+                                            int ItemsPerPage, 
+                                            int TotalItems)
+        {
+            var paginationHeader = new PaginationHeader(CurrentPage, TotalPages, ItemsPerPage, TotalItems);
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Header", "Pagination");
+        }
+    }
+}
---
+++ SmartSchool.API/Helper/PageList.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Helper
+{
+ 
+    //É uma classe que espera um objeto genérico e implementa uma lista generica para retorno;
+    public class PageList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public PageList(List<T> items, int count, int pageNumber, int pageSize) 
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }
+        public static async Task<PageList<T>> CreateAsync(IQueryable<T> source, int pageNumber,int pageSize)
+        {
+            //Aqui é onde é chamado a classe: É feito o tratamento de paginação em si, com os counts, as configurações e definições de páginas e quantidades de itens por páginas
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            //Aqui é onde basicamente essa classe é instanciada: Percebe-se que ela não é instanciada em sua chamada nas controllers e afins,
+            //é usado esse método estático que ele sim inicializa o construtor após configurar todo o pagination
+            return new PageList<T>(items,count,pageNumber,pageSize);
+        }
+    }
+}
